1ecae6867ecb950127992410fe73ce6614141759
Ananya154 <163438449+Ananya154@users.noreply.github.com>
2025-07-14 23:39:55 +0530

    Delete spring-rest-handson-3 directory

--- spring-rest-handson-3/src/main/java/com/cognizant/springresthandson3/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.springresthandson3.service;
-
-import com.cognizant.springresthandson3.model.Employee;
-import java.util.List;
-
-public interface EmployeeService {
-    List<Employee> getAllEmployees();
-    Employee getEmployee(int id);
-}
