32c7e956c5d997e9b291ff739d02a285ab024b4b
Ananya154 <163438449+Ananya154@users.noreply.github.com>
2025-07-27 11:21:45 +0530

    Add files via upload

---
+++ Week_6/blogapp4/src/Posts.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Post from './Post';
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false
+    };
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => this.setState({ posts: data.slice(0, 5) })) 
+      .catch(error => {
+        console.error("Error fetching posts:", error);
+        this.setState({ hasError: true });
+      });
+  }
+
+  componentDidCatch(error, info) {
+    alert("Something went wrong: " + error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong while loading posts.</h2>;
+    }
+
+    return (
+      <div>
+        <h2>Latest Blog Posts</h2>
+        {this.state.posts.map(post => (
+          <Post key={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
